0e4119b620e638ab5078042a58ce432fb31381b6
Iman Bains <imanjitbains@yahoo.com>
2021-07-15 13:27:37 -0700

    Updated server

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
